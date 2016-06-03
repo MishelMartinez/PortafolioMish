877272dc4c3dad4a9d3043d5e7892dbed5770872
MishelMartinez <lilian.mishel06@gmail.com>
2016-06-03 02:30:46 +0200

    Portfolio Mishel

---
+++ js/main.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+
+$("#two").hide();
+$("#proyects").hide();//texto proyects 
+   $("#circle-proyects").hide();
+//cambio de color en habilidades
+  $("#panel").hover(function(){
+        $("#flip2").addClass("learning2");
+    });
+   $("#panel").hover(function(){
+        $("#flip3").addClass("learning3");
+    });
+
+   $("#panel2").click(function(){
+        $("#flip").slideToggle("slow");
+
+    });
+
+   // bandera de conocmientos
+   $("#one").click(function(){
+        $("#two").slideToggle("slow");
+
+    });
+/*
+$(".hider").click(function(){
+        $(this).parents(".ex").hide("slow");
+    });
+/*/
+
+//scroll cambio de letra en recta
+    $(window).scroll(function() {
+    if ($(document).scrollTop() > 700 && $(document).scrollTop() < 1100 ) {
+      $(".ra ").addClass("change1"); //ra contiene p cambian de color section 2
+      $(".navbar").css("background-color", "#FCE5CC"); // nav cambia de color
+    } else {
+      $(".ra").removeClass("change1");
+    }
+  });
+    //texto de programming
+    $(window).scroll(function() {
+    if ($(document).scrollTop() > 700 && $(document).scrollTop() < 1100 ) {
+      $("#text-program").addClass("change-program"); //ra contiene p cambian de color section 2
+    } else {
+      $("#text-program").removeClass("change-program");
+    }
+  });
+//section proyect  $(this)$("#proyects").css("color", "red").slideUp(2000).slideDown(2000);
+ $(window).scroll(function(){
+    if ($(document).scrollTop()>1600) {
+      $("#proyects").css("color", "black").show();
+    }else{
+      $("#proyects").Hide();
+
+    }
+   
+  });
+
+ //circle-proyects
+ $(window).scroll(function(){
+    if ($(document).scrollTop()>1600) {
+      $("#circle-proyects").slideDown();
+    }if($(document).scrollTop()<1700){
+       $("#circle-proyects").Hide();
+    }
+   
+  });
+  //section contact rombos cambian 
+
+    $(window).scroll(function(){
+    if ($(document).scrollTop()>2360) {
+      $(".figure-rombo,#figure-rombo2").addClass("transf-rombo");
+    }else{
+      $(".figure-rombo,#figure-rombo2").removeClass("transf-rombo");
+    }
+   
+  });
+
+//section proyectos
+  $(window).scroll(function(){
+    if ($(document).scrollTop()>1100 && $(document).scrollTop() < 1300) {
+      $("#pp").css("background-color", "green");
+    }
+    else{
+      $("#pp").css("background-color", "blue");
+    }
+  });
+
+
+});  
+
+
+
